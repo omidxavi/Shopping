b6010c552f23f5b1ad70b5f2cc016f9d9466649d
omidxavi <mr.moradi1992@gmail.com>
2024-04-12 18:22:26 +0330

    Add Admin Theme to the project. the local host run needs to be fixed.

---
+++ Shop.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Web.Areas.Admin.Controllers;
+
+public class HomeController : AdminBaseController
+{
+
+    #region dashboard
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    #endregion
+}
--- Shop.Web/Controllers/SiteBaseController.cs
+++ Shop.Web/Controllers/SiteBaseController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Shop.Web.Controllers;
 
+[Authorize]
+[Area("User")]
+[Route("user")]
 public class SiteBaseController : Controller
 {
     protected string ErrorMessage = "ErrorMessage";
